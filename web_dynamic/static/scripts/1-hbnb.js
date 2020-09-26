d8bbdd1e46d4f3ffa5dfe4be95e88df6fcdd8bcc
IhebChatti <iheb.chatti@holbertonschool.com>
2020-09-26 16:28:55 +0100

    fix semistandard

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,11 +1,12 @@
+const $ = window.$;
 window.addEventListener('DOMContentLoaded', (event) => {
-	let amenityDict = {};
-	window.$('input[type=checkbox]').change(function () {
-		if (this.checked) {
-			amenityDict[$(this).attr("data-id")] = $(this).attr("data-name");
-		} else {
-			delete amenityDict[$(this).attr("data-id")];
-		}
-		window.$('.amenities h4').text(Object.values(amenityDict).join(', '))
-	})
+  const amenityDict = {};
+  $('input[type=checkbox]').change(function () {
+    if (this.checked) {
+      amenityDict[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenityDict[$(this).attr('data-id')];
+    }
+    $('.amenities h4').text(Object.values(amenityDict).join(', '));
+  });
 });
